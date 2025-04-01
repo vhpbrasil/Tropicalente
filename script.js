e007f978c39da6c65864420e2e3ab41c6a299889
vhpbrasil <vhpbrasil@gmail.com>
2025-04-01 20:27:17 -0300

    Obaaa

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+botaoDeAcessibilidade.addEventListener('click', function (){
+botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 
 
 })
