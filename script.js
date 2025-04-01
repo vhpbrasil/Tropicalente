4427731603bf05d8da365d11491ff542638f751a
vhpbrasil <vhpbrasil@gmail.com>
2025-04-01 20:29:53 -0300

    okok

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ document.addEventListener('DOMContentLoaded', function () {
         tamanhoAtualFonte -= 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
-const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
-const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
-
-botaoDeAcessibilidade.addEventListener('click', function (){
-botaoDeAcessibilidade.classList.toggle('rotacao-botao');
-opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
 
-})
+    botaoDeAcessibilidade.addEventListener('click', function(){
+
+
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+});
+
