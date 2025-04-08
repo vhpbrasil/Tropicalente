82a12548e3cf1fa94271227e101364b7914198de
vhpbrasil <vhpbrasil@gmail.com>
2025-04-08 20:51:52 -0300

    ok

--- script.js
+++ script.js
@@ -22,5 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
+
+    const alternaContraste = document.getElementById('alterna-contraste')
+
+    alternaContraste.addEventListener('click', function () {
+        document.body.classList.toggle('alto-contraste')
+    })
+    
 })
 
